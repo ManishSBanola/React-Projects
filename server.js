c359ac7537f34f6f368ab9f5879e802cfce3fde3
manishb <manish.banola@imaginea.com>
2019-12-19 17:18:09 +0530

    webpack configuration 5

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, ".", "dist")));
+app.use(express.static(__dirname + "/dist"));
 
 app.get(`*`, (req, res) => {
   res.sendFile(path.resolve(__dirname, "./dist/index.html"));
