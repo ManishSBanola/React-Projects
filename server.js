fa0d3a40e5865e8a742053b0d35d55b8badae381
manishb <manish.banola@imaginea.com>
2020-01-06 16:48:40 +0530

    webpack configuration 9

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
-app.get(`*`, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./dist/index.html"));
-  console.log(__dirname, "dirname");
+// app.get(`*`, (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./dist/index.html"));
+//   console.log(__dirname, "dirname");
+// });
+
+app.get("/health", (req, res) => {
+  res.json({ message: `ok` });
 });
+
 app.listen(port);
 console.log("server started");
