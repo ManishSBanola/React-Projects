de46b8004d8b0ac9a25f5a9a8671bef7a09f4b71
manishb <manish.banola@imaginea.com>
2020-01-07 14:47:16 +0530

    heroku build 6

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ const path = require("path");
 const webpack = require("webpack");
 const dotenv = require("dotenv").config({ path: __dirname + "/.env" });
 module.exports = {
-  entry: {
-    server: "./server.js"
-  },
   node: {
     __dirname: false
   },
